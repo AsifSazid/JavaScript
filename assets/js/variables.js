cc6a28592af77a17c66143d144de86c9117fa5b0
AsifSazid <pondit.asifsazid@gmail.com>
2024-04-29 17:13:17 +0600

    initial commit

---
+++ assets/js/variables.js
@@ -0,0 +1,43 @@
+// declaration
+//mzr // throws error
+
+
+// declaration & initialization
+ mzr = 'enter your name';
+console.log(mzr)
+
+// assignment
+mzr = 'Mian Zadid Rusdid'
+console.log(mzr)
+
+mzr = ' i am a tainer@Pondit'
+console.log(mzr)
+
+// 3 keywords 
+let number1 = 2
+var number2 = 3
+
+const PI = 3.14
+result = number1 + number2 + PI
+
+
+// How to display using javascript
+
+// 1. alert :
+alert(result)
+
+// 2. console : developer mode
+console.log(result)
+
+// 3. document.write() : unusal use
+document.writeln(result)
+document.write(result)
+
+// 4. innerHTML : use for output
+document.body.innerHTML = result
+document.getElementsByTagName('BODY')[0].innerHTML = 'Hello'
+
+// 5. append: use for output
+
+// clean code
+// scope: local & global
